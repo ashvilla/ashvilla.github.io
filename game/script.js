961d85e351fc658ed73db1ad0f46fc57858edd1c
ashvilla <90006595+ashvilla@users.noreply.github.com>
2022-03-17 14:48:09 -0400

    Delete game directory

--- game/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 800, timeBetweenNPCs: 10000, npcCollisionRadius: 100, actionDuration:600, jumpHeight: 300, makeResponsive: true, npcMoveToX: 990, npcSpeed: 200,   });
-}) ;
